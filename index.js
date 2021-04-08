0a9641dee73aeb47ecb01aabf7594cfce690365c
rlapsky <61615744+rlapsky@users.noreply.github.com>
2021-04-08 17:57:04 +0300

    end

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-new CountdownTimer({
+let getEl = x => document.querySelector(x)
+class CountdownTimer {
+    constructor({selector, targetDate}) {
+        this.selector = selector;
+        this.targetDate = targetDate;
+    }
+    time() {
+        let interval = setInterval(() => {
+            let time = new Date(this.targetDate) - new Date()
+            if (time <= 0) {
+                clearInterval(interval)
+                return;
+            } else {
+                const days = Math.floor(time / (1000 * 60 * 60 * 24));
+                const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+                const secs = Math.floor((time % (1000 * 60)) / 1000);
+                days < 10 ? getEl(`${this.selector} [data-value="days"]`).textContent = '0' + days : getEl(`${this.selector} [data-value="days"]`).textContent = days;
+                hours < 10 ? getEl(`${this.selector} [data-value="hours"]`).textContent = '0' + hours : getEl(`${this.selector} [data-value="hours"]`).textContent = hours;
+                mins < 10 ? getEl(`${this.selector} [data-value="mins"]`).textContent = '0' + mins : getEl(`${this.selector} [data-value="mins"]`).textContent = mins;
+                secs < 10 ? getEl(`${this.selector} [data-value="secs"]`).textContent = '0' + secs : getEl(`${this.selector} [data-value="secs"]`).textContent = secs;
+            }
+        }, 1000)
+    }
+}
+let test = new CountdownTimer({
     selector: '#timer-1',
-    targetDate: new Date('Jul 17, 2019'),
-  });
-
-  const days = Math.floor(time / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-  const secs = Math.floor((time % (1000 * 60)) / 1000);
+    targetDate: new Date('Apr 08, 2021, 17:58'),
+});
+test.time()
